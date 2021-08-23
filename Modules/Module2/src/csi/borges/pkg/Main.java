3558f740ef0414ac19babc4b26f54124620549dc
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-08-23 10:25:32 -0400

    completed module 2
    completed module 2

---
+++ Modules/Module2/src/csi/borges/pkg/Main.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package csi.borges.pkg;
+
+
+
+/**
+ * @author user
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub}
+		System.out.println("Hello, World");
+		System.out.println("Julio A. Borges Morales");
+		System.out.println("25-008");
+		System.out.println(2+5*2);
+	}
+}
+
+		
+
+
