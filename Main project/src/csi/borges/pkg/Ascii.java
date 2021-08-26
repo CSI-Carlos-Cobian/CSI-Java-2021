351ab7ac0d936c9624aa1a94df9f71aea9d369d5
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-08-26 09:36:14 -0400

    Ascii.java

---
+++ Main project/src/csi/borges/pkg/Ascii.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package csi.borges.pkg;
+
+/**
+ * @author user
+ *
+ */
+public class Ascii { 
+	public static void main(String[] args) {
+		
+			  
+	   System.out.println("         /?\\  ");
+	   System.out.println("         \\?/  ");
+	   System.out.println("          __  ");
+       System.out.println("         |??|");
+       System.out.println("         |??|");
+       System.out.println("         |??|");
+       System.out.println("         |??|");
+       System.out.println("         |??|");
+       System.out.println(" ___    /???/");
+       System.out.println("|???\\  /???/"); 		
+       System.out.println(" \\???\\/???/");
+       System.out.println("  \\______/");
+      
+       System.out.println(" __          __");
+      System.out.println(" |??|        |??|");  
+      System.out.println(" |??|        |??|");
+      System.out.println(" |??|        |??|");
+      System.out.println(" |??|        |??|");		
+      System.out.println(" |??|        |??|");
+      System.out.println(" \\??\\        |??| ");
+      System.out.println("  \\??\\       |??|");
+      System.out.println("   \\??\\      |??|");
+      System.out.println("    \\??\\____/??/");
+      System.out.println("     \\________/");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+}
