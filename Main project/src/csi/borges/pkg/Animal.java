f0eaa583f7b24072f78498e7bd09ba0de3c371bc
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-08-27 14:48:43 -0400

    Animal

---
+++ Main project/src/csi/borges/pkg/Animal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package csi.borges.pkg;
+
+/**
+ * @author user
+ *
+ */
+public class Animal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
