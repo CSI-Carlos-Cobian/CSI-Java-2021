97f12b55e564f04ed72fde7cdb3e99cb35ea327e
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-06 14:47:57 -0400

    Update Taxonomy.java

--- Main project/src/csi/borges/pkg/Taxonomy.java
+++ Main project/src/csi/borges/pkg/Taxonomy.java
@@ -30,11 +30,7 @@ public class Taxonomy {
 		
 	}
 	
-	  //  Getters and Setters  
-    public String getName(String species,String genus, String family, String order, String classs,String phylum,String kingdom,String domain ){
-        return getName();
-        
-    }
+	  //  Getters and Setters
     public void setName(String species, String genus, String family, String order, String classs,String phylum,String kingdom,String domain){
     	this.species = species;
 		this.genus = genus;
@@ -58,8 +54,118 @@ public class Taxonomy {
 		this.domain = domain;
     }
     
-    public String getName(String species, String genus, String family, String order, String classs,String phylum,String kingdom,String domain){
-        return name;
-    }
+   
+
+	/**
+	 * @return the species
+	 */
+	public String getSpecies() {
+		return species;
+	}
+
+	/**
+	 * @param species the species to set
+	 */
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	/**
+	 * @return the genus
+	 */
+	public String getGenus() {
+		return genus;
+	}
+
+	/**
+	 * @param genus the genus to set
+	 */
+	public void setGenus(String genus) {
+		this.genus = genus;
+	}
+
+	/**
+	 * @return the family
+	 */
+	public String getFamily() {
+		return family;
+	}
+
+	/**
+	 * @param family the family to set
+	 */
+	public void setFamily(String family) {
+		this.family = family;
+	}
+
+	/**
+	 * @return the order
+	 */
+	public String getOrder() {
+		return order;
+	}
+
+	/**
+	 * @param order the order to set
+	 */
+	public void setOrder(String order) {
+		this.order = order;
+	}
+
+	/**
+	 * @return the classs
+	 */
+	public String getClasss() {
+		return classs;
+	}
+
+	/**
+	 * @param classs the classs to set
+	 */
+	public void setClasss(String classs) {
+		this.classs = classs;
+	}
+
+	/**
+	 * @return the phylum
+	 */
+	public String getPhylum() {
+		return phylum;
+	}
+
+	/**
+	 * @param phylum the phylum to set
+	 */
+	public void setPhylum(String phylum) {
+		this.phylum = phylum;
+	}
+
+	/**
+	 * @return the kingdom
+	 */
+	public String getKingdom() {
+		return kingdom;
+	}
+
+	/**
+	 * @param kingdom the kingdom to set
+	 */
+	public void setKingdom(String kingdom) {
+		this.kingdom = kingdom;
+	}
+
+	/**
+	 * @return the domain
+	 */
+	public String getDomain() {
+		return domain;
+	}
+
+	/**
+	 * @param domain the domain to set
+	 */
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
    
 }
