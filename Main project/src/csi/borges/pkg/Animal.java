8d8ddfbf90ca4b96ddc2c324ccb2d669c096ab0e
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-08 08:46:49 -0400

    Update Animal.java

--- Main project/src/csi/borges/pkg/Animal.java
+++ Main project/src/csi/borges/pkg/Animal.java
@@ -1,82 +1,97 @@
 package csi.borges.pkg;
 
-public class Animal{
-		//	Fields
-        String name;
-        int age;
-        String scientificName;
-        Taxonomy taxonomy;
-    //  Constructors
-    	public Animal(String name,int age,String scientificName,String Taxonomy) {
-    		this.name = name;
-    		this.age = age;
-    		this.scientificName = scientificName;
-    		
-    	}
-    	public Animal() {}
+public class Animal {
+	// Fields
+	String name;
+	int age;
+	String scientificName;
+	Taxonomy taxonomy;
 
-        //  Getters and Setters  
-        public String getName(){
-            return name;
-        }
-        public void setName(String name){
-            this.name = name;
-        }
-        
-        public int getAge(){
-            return age;
-        }
-        public void setAge(int age){
-            this.age = age;
-        }
-        
-        public String getScientificName(){
-            return scientificName;
-        }
-        public void setScientificName(String scientificName){
-            this.scientificName = scientificName;
-        }
-            //  Main Method 
-            public static void main(String[] args){
-            	Animal a = new Animal(); 
-                a.setName("Koala");
-                a.setAge(13);  
-                a.setScientificName("Phascolarctos cinereus");
-                
-                System.out.println("Name: " + a.getName());
-                System.out.println("Age: " + a.getAge());
-                System.out.println("Scientific Name: " + a.getScientificName());
-           
-               
-                Animal a2 = new Animal("Parrot",50,"Psittaciformes","phylum");
+	// Constructors
+	public Animal(String name, int age, String scientificName, String Taxonomy) {
+		this.name = name;
+		this.age = age;
+		this.scientificName = scientificName;
 
-                
-                System.out.println("Name: " + a2.getName());
-                System.out.println("Age: " + a2.getAge());
-                System.out.println("Scientific Name: " + a2.getScientificName());
-               
-                Animal a3 = new Animal("Horse",3,"Equus caballus","species");
-               
-                System.out.println("Name: " + a3.getName());
-                System.out.println("Age: " + a3.getAge());
-                System.out.println("Scientific Name: " + a3.getScientificName());
-                
-                a3.getTaxonomy().getOrder(); 
-            }
-            public Animal(String name,int age, String scientificName) {
-        		this.name = name;
-        		this.age = age;
-        		this.scientificName = scientificName;
-            }
-            
-        		  public Animal(String name,int age, String scientificName, Taxonomy taxonomy) {
-              		this.name = name;
-              		this.age = age;
-              		this.scientificName = scientificName;
-              		this.setTaxonomy(taxonomy);
-        		  }
-        		  
-        		  public Animal() {
+	}
+
+	public Animal() {
+	}
+
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getScientificName() {
+		return scientificName;
+	}
+
+	public void setScientificName(String scientificName) {
+		this.scientificName = scientificName;
+	}
+
+	// Main Method
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		a.setName("Koala");
+		a.setAge(13);
+		a.setScientificName("Phascolarctos cinereus");
+
+		System.out.println("Name: " + a.getName());
+		System.out.println("Age: " + a.getAge());
+		System.out.println("Scientific Name: " + a.getScientificName());
+
+		Animal a2 = new Animal("Parrot", 50, "Psittaciformes", "phylum");
+
+		System.out.println("Name: " + a2.getName());
+		System.out.println("Age: " + a2.getAge());
+		System.out.println("Scientific Name: " + a2.getScientificName());
+
+		Animal a3 = new Animal("Horse", 3, "Equus caballus", "species");
+
+		System.out.println("Name: " + a3.getName());
+		System.out.println("Age: " + a3.getAge());
+		System.out.println("Scientific Name: " + a3.getScientificName());
+
+		a2.getTaxonomy().getOrder();
+		a3.getTaxonomy().getOrder();
+	}
+
+	private Taxonomy getTaxonomy() {
+		return null;
+	}
+
+	public Animal(String name, int age, String scientificName) {
+		this.name = name;
+		this.age = age;
+		this.scientificName = scientificName;
+	}
+
+	public Animal(String name, int age, String scientificName, Taxonomy taxonomy) {
+		this.name = name;
+		this.age = age;
+		this.scientificName = scientificName;
+		this.setTaxonomy(taxonomy);
+	}
+
+	private void setTaxonomy(Taxonomy taxonomy) {
+
+	}
+
+	public class Animal() {
         		  }
         		  public Taxonomy getTaxonomy() {
         			  return taxonomy; 
