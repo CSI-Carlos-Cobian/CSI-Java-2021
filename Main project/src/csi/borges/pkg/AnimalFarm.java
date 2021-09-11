88d19e10df1d8cbf787eb04166eb7c1e55f65a70
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-11 17:38:18 -0400

    Update AnimalFarm.java

--- Main project/src/csi/borges/pkg/AnimalFarm.java
+++ Main project/src/csi/borges/pkg/AnimalFarm.java
@@ -9,4 +9,15 @@ package csi.borges.pkg;
  */
 public class AnimalFarm {
 
+	static Animal[]animals; 
+	
+	public static void main(string[] args) {
+	
+		
+		animals= new Animal[5];
+	animals[0]= new Animal ("Horse", 3, "Equus caballus", new Taxonomy("Equus caballus","Equus Linnaeus","Equidae Gray","Perissodactyla Owen","Mammalia Linnaeus","	Chordata","	Animalia","Eukaryote"));
+	animals[0]= new Animal ("Parrot",50, "Psittaciformes", new Taxonomy("Psittaciformes","Bavaripsitta" , "	Psittacidae","Psittaciformes", "Aves", 	"Chordata", 	"Animalia", "	Bilateria "));
+		
+		
+	}
 }
