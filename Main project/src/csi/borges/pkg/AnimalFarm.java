b21d5a563dddb0015340c72d571322cd761e0419
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-17 14:39:38 -0400

    Update AnimalFarm.java

--- Main project/src/csi/borges/pkg/AnimalFarm.java
+++ Main project/src/csi/borges/pkg/AnimalFarm.java
@@ -11,7 +11,7 @@ public class AnimalFarm {
 	animals[0]= new Animal ("Horse", 3, "Equus caballus", new Taxonomy("Equus caballus","Equus Linnaeus","Equidae Gray","Perissodactyla Owen","Mammalia Linnaeus","	Chordata","	Animalia","Eukaryote"));
 	animals[2]= new Animal ("Parrot",50, "Psittaciformes", new Taxonomy("Psittaciformes","Bavaripsitta" , "	Psittacidae","Psittaciformes", "Aves", 	"Chordata", 	"Animalia", "	Bilateria "));
 	animals[3]= new Animal ("Shark",5, "Selanchimorpha", new Taxonomy("Eukaryote", "Animalia", "Chordata", "Chondrichthyes", "Lamniformes", "Lamnidae", "Carcharodon carcharias"));
-	animals[3]= new Animal ("Parrot",50, "Psittaciformes", new Taxonomy("Psittaciformes","Bavaripsitta" , "	Psittacidae","Psittaciformes", "Aves", 	"Chordata", 	"Animalia", "	Bilateria "));
+	animals[3]= new Animal ("Lion",5, "Panthera leo", new Taxonomy("Eukaryote","Animalia" , "Chordate","Mammal", "Carnivora", 	"Felidae", 	"Panthera", " Panthera leo "));
 	animals[4]= new Animal ("Parrot",50, "Psittaciformes", new Taxonomy("Psittaciformes","Bavaripsitta" , "	Psittacidae","Psittaciformes", "Aves", 	"Chordata", 	"Animalia", "	Bilateria "));
 	animals[5]= new Animal ("Parrot",50, "Psittaciformes", new Taxonomy("Psittaciformes","Bavaripsitta" , "	Psittacidae","Psittaciformes", "Aves", 	"Chordata", 	"Animalia", "	Bilateria "));
 	animals[6]= new Animal ("Parrot",50, "Psittaciformes", new Taxonomy("Psittaciformes","Bavaripsitta" , "	Psittacidae","Psittaciformes", "Aves", 	"Chordata", 	"Animalia", "	Bilateria "));
