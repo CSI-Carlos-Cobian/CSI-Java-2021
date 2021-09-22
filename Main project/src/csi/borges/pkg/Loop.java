c71dab754ab8e3c7f3c26ee56313b2cfeefc0978
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-22 09:15:59 -0400

    Update Loop.java

--- Main project/src/csi/borges/pkg/Loop.java
+++ Main project/src/csi/borges/pkg/Loop.java
@@ -9,10 +9,27 @@ public class Loop {
 			
 			System.out.println(i);
 		}
-			int[][] arr = {{1, 2}, {3, 4}}; 
-			int[][] arr2 =  {{1, 2, 3, 4, 5}, {6,7,8,9,1}, { 2, 3, 4, 5, 6}}; 
+			int[][] arr = {
+					{1, 2}, 
+					{3, 4}}; 
+			int[][] arr =  {
+					{1, 2, 3, 4, 5}, 
+					{6, 7, 8, 9, 10},
+					{11, 12, 13, 14, 15}
+					}; 
+			System.out.println((arr[2][4] ));
 			System.out.println(Arrays.toString(arr[0] ));
 			
+			nestedPrint(arr); 
 	}
-	
-}
+	public static void nestedPrint(int[][] arr) {
+		
+		for(int i = 0; i <arr.length; i++) {
+			for(int  j = 0 ; j< arr[i]. length; j++) {
+				System.out.println(arr[1][j]); 
+				
+				}
+			}
+		}
+	}
+
