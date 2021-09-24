07a2569d91528e5f87077c1dda28cbf971f9ecc1
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-24 13:56:29 -0400

    Update EvenOdd2D.java

--- Main project/src/csi/borges/pkg/EvenOdd2D.java
+++ Main project/src/csi/borges/pkg/EvenOdd2D.java
@@ -7,9 +7,11 @@ public class EvenOdd2D {
 		int[][] arr = new int[10][10];  
 		for(int i = 0; i < arr.length; i++) {
 		    for(int j = 0; j < arr[i].length; j++) {
+		    	if (i%2==1); 
 		    	
 		    	System.out.println(Arrays.toString(arr[i]));
-			    System.out.println(arr[i][j]); 
+			    System.out.println(arr[i]); 
+			    
 	
 	}
  
