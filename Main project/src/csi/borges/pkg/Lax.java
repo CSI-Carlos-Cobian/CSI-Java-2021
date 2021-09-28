2c45a548918742278b45d21001794c05a2072f5b
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-28 09:27:27 -0400

    Update Lax.java

--- Main project/src/csi/borges/pkg/Lax.java
+++ Main project/src/csi/borges/pkg/Lax.java
@@ -1,11 +1,30 @@
 package csi.borges.pkg;
 
+import java.util.Arrays;
+
 public class Lax {
+	
 
 	public static void main (String[] args) {
-		int[][] arr = new int[10][10];  
+		int[][] arr = new int[9][9];  
 		
 		for(int i = 0; i < arr.length; i++) {
 		    for(int j = 0; j < arr[i].length; j++) {
+
+		    	
+		    	arr [i][i] = 1; 
+		      	arr [j][j] = 1-0 ;
+		      	
+		    
+		    	
+		    
+		    	
+		    			       
+		    }
+		    System.out.println(Arrays.toString(arr[i]));
+
+		}
+	}
 }
+		
 
