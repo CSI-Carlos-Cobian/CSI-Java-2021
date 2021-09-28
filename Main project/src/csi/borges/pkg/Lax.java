1ff095aebb8366ba2b5b34f8cacc1272f6addbef
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-28 08:50:53 -0400

    Update Lax.java

--- Main project/src/csi/borges/pkg/Lax.java
+++ Main project/src/csi/borges/pkg/Lax.java
@@ -2,4 +2,10 @@ package csi.borges.pkg;
 
 public class Lax {
 
+	public static void main (String[] args) {
+		int[][] arr = new int[10][10];  
+		
+		for(int i = 0; i < arr.length; i++) {
+		    for(int j = 0; j < arr[i].length; j++) {
 }
+
