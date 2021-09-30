7b0bf5eb48d1080699f3861bc66e0396d80a8f51
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-09-30 08:51:26 -0400

    Update Lax3D.java

--- Main project/src/csi/borges/pkg/Lax3D.java
+++ Main project/src/csi/borges/pkg/Lax3D.java
@@ -1,7 +1,26 @@
 package csi.borges.pkg;
 
+import java.util.Arrays;
+
 public class Lax3D {
 
 	int[][][] arr = new int [2][9][9]; 
+
+		for(int i = 0;i<arr.length;i++) {
+			System.out.println("--------------------------------");
+			
+			for(int j = 0; j < arr[i].length; j++) {
+				
+			    for(int k = 0; k < arr[i][j].length; k++) {
+	
+			    	arr[i][j][j] = 1;
+			    	arr[i][j][ arr[i][j].length - j - 1 ] = 1;
+			    }
+			    System.out.println(Arrays.toString(arr[i][j]));
+			    
+			}
+			
+		}
+	}
 	
 }
