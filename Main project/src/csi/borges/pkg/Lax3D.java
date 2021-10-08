7b00b0e944dee686d375f39c4aa558f12d2ef336
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-10-08 08:40:15 -0400

    Update Lax3D.java

--- Main project/src/csi/borges/pkg/Lax3D.java
+++ Main project/src/csi/borges/pkg/Lax3D.java
@@ -4,23 +4,27 @@ import java.util.Arrays;
 
 public class Lax3D {
 
-	int[][][] arr = new int [2][9][9]; 
+	int[][][] arr = new int[2][9][9];
 
-		for(int i = 0;i<arr.length;i++) {
-			System.out.println("--------------------------------");
-			
-			for(int j = 0; j < arr[i].length; j++) {
-				
-			    for(int k = 0; k < arr[i][j].length; k++) {
-	
-			    	arr[i][j][j] = 1;
-			    	arr[i][j][ arr[i][j].length - j - 1 ] = 1;
-			    }
-			    System.out.println(Arrays.toString(arr[i][j]));
-			    
-			}
-			
-		}
+	System.out.println("--------------------------------");
+	for (int j = 0; j < arr[0].length; j++) {
+	  for (int k = 0; k < arr[0][j].length; k++) {
+
+	    // X Logic Here
+	    // Can be done in 2 lines
+
+	  }
+	  System.out.println(Arrays.toString(arr[0][j]));
+
 	}
-	
-}
+
+	System.out.println("--------------------------------");
+	for (int j = 0; j < arr[1].length; j++) {
+	  for (int k = 0; k < arr[1][j].length; k++) {
+
+	    // Inverted X Logic Here
+	    // Can be done in 1 line
+
+	  }
+	  System.out.println(Arrays.toString(arr[1][j]));
+	}
