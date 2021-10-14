0f0d31c68b807b94cd37022d9e32b223f7596f28
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-10-14 09:29:53 -0400

    Update Rockpsperscissors.java

--- Main project/src/csi/borges/pkg/Rockpsperscissors.java
+++ Main project/src/csi/borges/pkg/Rockpsperscissors.java
@@ -7,30 +7,29 @@ import java.util.Scanner;
 
 public class Rockpsperscissors {
 
-		public static void main(String[] args) 
-	{
-	 
-		String[] choices = {"Rock", "Paper", "Scissors"}; 
-			Random r= new Random(); 
-			
-			//int randomnumber= r.nextInt(arr.length); 
-			String computerChoice = choices[ r.nextInt(choices.length)]; 
-			
-			Scanner sc= new Scanner(System.in); 
-			
+	public static void main(String[] args) {
+
+		String[] choices = { "Rock", "Paper", "Scissors" };
+		Random r = new Random();
+
+		// int randomnumber= r.nextInt(arr.length);
+		String computerChoice = choices[r.nextInt(choices.length)];
+
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("type your choice: ");
 		int inputInt = Arrays.asList(choices).indexOf(sc.nextLine());
-			String userChoice = choices[inputInt]; 
-			
-			System.out.println("computer chose:  " +computerChoice); 
-			System.out.println("User chose: " + userChoice); 
-			
-			if(computerChoice == userChoice ) {
-				System.out.println("It's a Tie!");
-			}
-			else if(computerChoice == choices[0] && userChoice == choices[1]) {
-				System.out.println("You Win!"); 
-			}
-			
-}
+		String userChoice = choices[inputInt];
+
+		System.out.println("computer chose:  " + computerChoice);
+		System.out.println("User chose: " + userChoice);
+
+		if (computerChoice == userChoice) {
+			System.out.println("It's a Tie!");
+		} else if (computerChoice == choices[0] && userChoice == choices[1]) {
+			System.out.println("You Win!");
+		} else {
+
+		}
 	}
+}
