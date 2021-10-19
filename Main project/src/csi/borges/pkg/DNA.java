10b10f5a1019cdd0faa320a57b39fcc6596c26b6
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-10-19 18:17:37 -0400

    Create DNA.java

---
+++ Main project/src/csi/borges/pkg/DNA.java
@@ -0,0 +1,29 @@
+package csi.borges.pkg;
+
+import java.util.Arrays;
+
+public class DNA {
+
+public static void main (String []args) throws InterruptedException{
+		
+		
+		
+		while (true) {
+			int [][] arr = new int [9][9]; 
+			
+			for(int i = 0; i< arr.length; i++) {
+				
+				for(int j = 0; j<arr[i].length; j++) {
+					
+					arr[i][i]= 1; 
+					arr[i][ arr[i].length -i -1 ] = 1; 
+					Thread.sleep(75);
+					
+					
+					System.out.println(Arrays.toString(arr[i])); 
+				}
+			}
+		}
+	}
+	
+}
