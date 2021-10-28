90c6ce60cdab6a231ff0bf12fb9bb89a9a2a94f9
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-10-28 18:48:20 -0400

    Update Calculator.java

--- Main project/src/csi/borges/life/Calculator.java
+++ Main project/src/csi/borges/life/Calculator.java
@@ -3,12 +3,65 @@ package csi.borges.life;
 public class Calculator {
 	public static void main(String[] args) throws InterruptedException {
 		
-		double initial= 4000; 
+		double initial= 6000; 
 		double cash= initial; 
 		
 		double debt= 0;
-		double invested= 500
+		double invested= 500; 
+		
+		for( int age= 0; age<= 81; age++) {
+			
+			if (age <17) {// highschool 
+				
+				String location= "PR"; 
+				double salary= 0; 
+				
+				cash += salary; 
+				
+			} else if ( age <19) { //// Penn state University 
+				
+				String location = "PA"; 
+				double salary= 29_000;
+				double tuition= 35_000+ 45_000-15_000; 
+				double costOfLiving= 37_000; 
+				
+				double LoanInterest = 0.00; 
+				
+				cash+= salary; 
+				debt-= tuition; 
+				debt-= costOfLiving; 
+				debt+= ({LoanInterest * debt) * 12);
+				
+				} else if (age <24 ) { //residency 
+					
+					String location= "PR"
+				}
 				
 				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			}
+		}
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
