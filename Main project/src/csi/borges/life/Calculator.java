4f92aeed9da7725b1b04b3edc04ed142ddf4825f
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-10-28 20:30:23 -0400

    Update Calculator.java

--- Main project/src/csi/borges/life/Calculator.java
+++ Main project/src/csi/borges/life/Calculator.java
@@ -34,12 +34,12 @@ public class Calculator {
 				
 				} else if (age <24 ) { //residency 
 					
-					String location= "PA"
+					String location= "PA";
 					double salary = 68_000; 
 					double tuition = 200; 
 					double costOfLiving= 40_000; 
 					double Investment = 5_000; 
-					double rent= 12* (2_000*500)
+					double rent= 12* (2_000*500); 
 					double food= 20*365; 
 					double InvestmentInterest= 1.00; 
 					double LoanInterest= 0.00; 
@@ -50,6 +50,53 @@ public class Calculator {
 					debt += ({LoanInterest* debt)*12); 
 					
 					} else if (age <38) {//work 
+						
+						String location= "PR"; 
+						double salary= 125_000; 
+						double costOfLiving= 50_000; 
+						double investment= 10_000; 
+						double food= 500*12;
+						double InvestmentInterest= 1.00;
+						double LoanInterest= 0.00; 
+						double carAndExpenses= 20_000 + 200 *12 *2000 + 1000 *12; 
+						double rent = 12*(1600+500); 
+						
+						cash += salary; 
+						debt-= food; 
+						debt-= costOfLiving;
+						debt+= ({LoanInterest * debt)* 12; 
+						debt-= carAndExpenses; 
+						
+						
+						} else if (age<65) (//retire
+								// salary - 12% tax 
+								double salary = 200_000 * 1.0; 
+								
+								// 2 bedroom house with 2 bathrooms + electric/ water/ wi-fi bills
+								double rent = 12*(2000+ 600); 
+								
+								// 30$ daily food stipend 
+								double food= 50*365; 
+								
+								// 100 monthly dollars o gas 
+								// 4,000 a year on car fixes
+								//montly car payment 500
+								double carAndExpenses= 200 * 12 + 2_000 * 1_000 * 12; 
+								
+								cash+= salary; 
+								cash-= food; 
+								cash-= rent;
+								cash-= carAndExpenses; 
+					} else if (age< 80) {// Living the dream 
+					}
+					System.out.println("Balance at age: "+ age + "is"+ cash + "with a debt of" + debt+ )
+								
+								
+								
+								
+								
+								
+					}
 					
 				}
 				
@@ -62,7 +109,7 @@ public class Calculator {
 				
 				
 				
-				
+			
 			}
 		}
 			
