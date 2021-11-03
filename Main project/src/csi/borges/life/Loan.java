d67efd60bde66119ac08b23a823a4da93e6ecb3d
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-11-03 09:14:02 -0400

    Create Loan.java

---
+++ Main project/src/csi/borges/life/Loan.java
@@ -0,0 +1,22 @@
+package csi.borges.life;
+
+public class Loan {
+String name;
+
+double balance; 
+double rate; 
+double paymentAmount; 
+
+int startAge; 
+
+Public Loan (String name, double balance, double rate, double paymentAmount, )
+super(); 
+
+
+
+
+
+
+
+
+}
