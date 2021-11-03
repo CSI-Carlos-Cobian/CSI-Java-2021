2596baee8e8b4d7b6d6b684fae189eb15148472e
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-11-03 09:21:37 -0400

    Update Loan.java

--- Main project/src/csi/borges/life/Loan.java
+++ Main project/src/csi/borges/life/Loan.java
@@ -1,22 +1,39 @@
 package csi.borges.life;
 
 public class Loan {
-String name;
+	String name;
 
-double balance; 
-double rate; 
-double paymentAmount; 
+	double balance;
+	double rate;
+	double paymentAmount;
 
-int startAge; 
+	int startAge;
 
-Public Loan (String name, double balance, double rate, double paymentAmount, )
-super(); 
+	public Loan(String name, double balance, double rate, double paymentAmount, int startAge) {
+		super();
+		this.name = name;
+		this.balance = balance;
+		this.rate = rate;
+		this.paymentAmount = paymentAmount;
+		this.startAge = startAge;
+	}
+	
+	public double makePayment() {
+		balance-= paymentAmount; 
+		
+		if( balance < 0) {
+			double actualPayment = paymentAmount + balance; 
+			balance =0; 
+			return actualPayment; 
+		}
+		return paymentAmount; 
+		
+	}
+public void accureInterest() {
+	balance += balance * rate; 
+}
 
 
-
-
-
-
-
-
+	
+	
 }
