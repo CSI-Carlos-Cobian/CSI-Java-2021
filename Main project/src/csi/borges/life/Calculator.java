d651be95e84e4ddfe35c37fb7e9ebe7392a4385b
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-11-09 09:31:09 -0400

    Update Calculator.java

--- Main project/src/csi/borges/life/Calculator.java
+++ Main project/src/csi/borges/life/Calculator.java
@@ -1,6 +1,5 @@
 package csi.borges.life;
 
-import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +12,7 @@ public class Calculator {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		double initial = 10_000;
+		double initial = 15_000;
 		double cash = initial;
 
 		double invested = 500;
@@ -31,38 +30,51 @@ public class Calculator {
 
 				String location = "PR";
 				double salary = 0;
-
+				double Investment = 1_000; 
 				cash += salary;
+				cash+= Investment; 
 
 			} else if (age < 19) { //// Penn state University
 
 				String location = "PA";
-				double salary = 29_000;
-				double tuition = 35_000 + 45_000 - 15_000;
-				double costOfLiving = 37_000;
-
-				double LoanInterest = 0.00;
+				double salary = 46_000;
+				double tuition = 25_000 + 35_000 - 15_000;
+				double costOfLiving = 15_000;
+				double Investment = 5_000; 
+				double LoanInterest = 1.00;
 
 				cash += salary;
-				cash += askForMoney(tuition);
+				cash+= Investment;
+				cash += askForMoney(tuition/2);
 				cash -= tuition;
-				cash += askForMoney(costOfLiving);
+				cash += askForMoney(costOfLiving/2);
 				cash -= costOfLiving;
 				cash += (LoanInterest * cash) * 12;
+				
+				loans.get(0).balance += costOfLiving/2;
+				loans.get(0).balance += tuition/2;
 
 			} else if (age < 24) { // residency
 
 				String location = "PA";
-				double salary = 68_000;
+				double salary = 75_000;
 				double tuition = 200;
-				double costOfLiving = 40_000;
+				double costOfLiving = 30_000;
 				double Investment = 5_000;
-				double rent = 12 * (2_000 * 500);
+				double rent = 12 * (1_100 * 350);
 				double food = 20 * 365;
 				double InvestmentInterest = 1.00;
-				double LoanInterest = 0.00;
+				double LoanInterest = 1.00;
 
 				cash += salary;
+				cash+= Investment; 
+				cash -= food;
+				 cash -= rent;
+				cash -= costOfLiving;
+				cash -= InvestmentInterest;
+				cash-= LoanInterest; 
+				
+
 				loans.get(0).balance += costOfLiving;
 				loans.get(0).balance += tuition;
 
@@ -77,12 +89,15 @@ public class Calculator {
 				String location = "PR";
 				double salary = 125_000;
 				double costOfLiving = 50_000;
+				double Investment = 10_000;
 				double food = 500 * 12;
 				double carAndExpenses = 20_000 + 200 * 12 * 2000 + 1000 * 12;
 				double rent = 12 * (1600 + 500);
 
 				cash += salary;
+				cash += Investment; 
 				cash -= food;
+				cash -= rent;
 				cash -= costOfLiving;
 				cash -= carAndExpenses;
 
@@ -91,15 +106,22 @@ public class Calculator {
 				double salary = 200_000 * 1.0;
 
 				// 2 bedroom house with 2 bathrooms + electric/ water/ wi-fi bills
-				double rent = 12 * (2000 + 600);
+				double rent = 12 * (1100 + 350);
 
 				// 30$ daily food stipend
-				double food = 50 * 365;
+				double food = 35 * 365;
 
 				// 100 monthly dollars o gas
 				// 4,000 a year on car fixes
 				// montly car payment 500
-				double carAndExpenses = 200 * 12 + 2_000 * 1_000 * 12;
+				double carAndExpenses = 150 * 12 + 1_000;
+				
+				 cash += salary;
+				  cash -= food;
+				  cash -= rent;
+				  cash -= carAndExpenses;
+				
+				
 			} else {// Living the dream
 			}
 			
