fad31db69ca0ddc635207685d2185259a53def61
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2021-11-11 08:55:41 -0400

    Update Calculator.java

--- Main project/src/csi/borges/life/Calculator.java
+++ Main project/src/csi/borges/life/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 	private static double askForMoney(double amount) {
 		System.out.println("You have been spared by someone who gave you: " + amount);
 		return amount;
-
+			
 	}
 
 	public static void main(String[] args) throws InterruptedException {
@@ -15,11 +15,11 @@ public class Calculator {
 		double initial = 15_000;
 		double cash = initial;
 
-		double invested = 500;
+		double invested = 5_000;
 
 		List<Loan> loans = new ArrayList<Loan>();
 
-		loans.add(new Loan("Student Loan", 0, 0.0466, 0, 23));
+		loans.add(new Loan("Student Loan", 0, 0.0466, 0, 10));
 		loans.add(new Loan("Personal Loans", 0, 0.16, 0, 0));
 		loans.add(new Loan("Mortgage Loans", 0, 0.03125, 0, 0));
 		loans.add(new Loan("CreditCard", 0, 0.29, 0, 0));
@@ -29,19 +29,19 @@ public class Calculator {
 			if (age < 17) {// highschool
 
 				String location = "PR";
-				double salary = 0;
-				double Investment = 1_000; 
+				double salary = 4_000;
+				double Investment = 5_000; 
 				cash += salary;
 				cash+= Investment; 
 
 			} else if (age < 19) { //// Penn state University
 
 				String location = "PA";
-				double salary = 46_000;
-				double tuition = 25_000 + 35_000 - 15_000;
-				double costOfLiving = 15_000;
-				double Investment = 5_000; 
-				double LoanInterest = 1.00;
+				double salary = 50_000;
+				double tuition = 25_000 - 15_000;
+				double costOfLiving = 12_000;
+				double Investment = 16_000; 
+				double LoanInterest = 1.0;
 
 				cash += salary;
 				cash+= Investment;
@@ -57,10 +57,10 @@ public class Calculator {
 			} else if (age < 24) { // residency
 
 				String location = "PA";
-				double salary = 75_000;
+				double salary = 80_000;
 				double tuition = 200;
-				double costOfLiving = 30_000;
-				double Investment = 5_000;
+				double costOfLiving = 24_000;
+				double Investment = 41_000;
 				double rent = 12 * (1_100 * 350);
 				double food = 20 * 365;
 				double InvestmentInterest = 1.00;
@@ -89,7 +89,7 @@ public class Calculator {
 				String location = "PR";
 				double salary = 125_000;
 				double costOfLiving = 50_000;
-				double Investment = 10_000;
+				double Investment = 65_000;
 				double food = 500 * 12;
 				double carAndExpenses = 20_000 + 200 * 12 * 2000 + 1000 * 12;
 				double rent = 12 * (1600 + 500);
@@ -104,7 +104,7 @@ public class Calculator {
 			} else if (age < 65) { // retire
 				// salary - 12% tax
 				double salary = 200_000 * 1.0;
-
+				double Investment = 100_000; 
 				// 2 bedroom house with 2 bathrooms + electric/ water/ wi-fi bills
 				double rent = 12 * (1100 + 350);
 
@@ -117,6 +117,7 @@ public class Calculator {
 				double carAndExpenses = 150 * 12 + 1_000;
 				
 				 cash += salary;
+				 cash += Investment; 
 				  cash -= food;
 				  cash -= rent;
 				  cash -= carAndExpenses;
