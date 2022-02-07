3ef6531021025935ed9bbefa7e2d60ad54129c1b
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-07 08:51:36 -0400

    Update Dog.java

--- Main project/src/csi/borges/inheritance/Dog.java
+++ Main project/src/csi/borges/inheritance/Dog.java
@@ -8,5 +8,14 @@ package csi.borges.inheritance;
  *
  */
 public class Dog {
+String pelo; 
+int size ; 
+boolean wild= false; 
+
+void piss() {}
+void eat(food){return null;}
+void die(){}
+void Bark() {return null;}
+void wagtail() {}
 
 }
