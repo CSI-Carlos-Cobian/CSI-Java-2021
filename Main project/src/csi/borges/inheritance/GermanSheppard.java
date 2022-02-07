8158b5e20907f7f94727bf458ed9a76e530102a1
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-07 09:04:50 -0400

    Create GermanSheppard.java

---
+++ Main project/src/csi/borges/inheritance/GermanSheppard.java
@@ -0,0 +1,14 @@
+package csi.borges.inheritance;
+
+public class GermanSheppard extends Dog {
+
+	this.size = 88; 
+	this.wild = false; 
+	@Override
+	@Override 
+	void NoiseBark(){} 
+	
+	return new Noise(Loud);
+}
+
+To String()
