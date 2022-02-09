ca59d970d0972ba0720876b2f44093d32f82d691
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-09 09:01:32 -0400

    Update Dog.java

--- Main project/src/csi/borges/inheritance/Dog.java
+++ Main project/src/csi/borges/inheritance/Dog.java
@@ -1,11 +1,13 @@
 package csi.borges.inheritance;
 
 public class Dog {
-	String pelo;
+	String hair;
 	int size;
 	boolean wild = false;
+	boolean gender= true; 
 
-	public void piss() {
+	public void piss(Food f) {
+		return f.digest(); 
 	}
 
 	public Shit eat() {
@@ -33,15 +35,34 @@ public class Dog {
 		}
 		
 	}
-
-	public class Noise {
-		int loud; 
-		int annoying;
-		int continuous; 
+	public void Piss() {
+		if(gender==true) {
+			System.out.println("raise leg");
+		}
+		System.out.println("Tsssss");
 		
-		
+	}
 
+	public class Noise Bark(){
+		return new Noise();
+	}
+	public void Die() {}
+	public void wagTail() {}
+	public void mate(Dog D) {
+	}
 		public Noise() {
+			
+		}
+		
+	
+	public class Food {
+		int tasty; 
+		int colorful;
+		int sweet; 
+		
+		
+
+		public Food() {
 			
 		}
 		
