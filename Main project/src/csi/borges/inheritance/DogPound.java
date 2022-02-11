ce71b3fd7ae8f50e16a950a81b23f53f25b5a8b4
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-11 09:12:59 -0400

    Update DogPound.java

--- Main project/src/csi/borges/inheritance/DogPound.java
+++ Main project/src/csi/borges/inheritance/DogPound.java
@@ -1,10 +1,33 @@
 
 package csi.borges.inheritance;
 
-import javax.swing.JFrame;
 
-public class DogPound extends JFrame {
+import java.awt.Color;
+import java.awt.Dimension;
 
-	Main(){}
-	Dog [] dogs
-}
+import javax.swing.JPanel;
+
+public class DogPound extends JPanel {
+
+	 private void initDogPound() {
+
+	        addKeyListener(new TAdapter());
+	        setBackground(new Color(0, 131, 22));
+	        setFocusable(true); 
+
+	        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	        loadImages();
+	        initGame();
+	 }
+
+	private void initGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void loadImages() {
+		// TODO Auto-generated method stub
+		
+	}
+	    }
+
