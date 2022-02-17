f85fd7ca655a3fc83f137c7284d2f870d48c7e31
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-17 09:00:46 -0400

    Update GermanSheppard.java

--- Main project/src/csi/borges/inheritance/GermanSheppard.java
+++ Main project/src/csi/borges/inheritance/GermanSheppard.java
@@ -1,5 +1,11 @@
 package csi.borges.inheritance;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import csi.borges.snake.Board;
+
 public class GermanSheppard extends Dog {
 
 	public GermanSheppard() {
@@ -18,4 +24,36 @@ public class GermanSheppard extends Dog {
 	public Noise bark() {
 		return new Noise();
 	}
+	class simulation extends JFrame {
+	/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+	public void Dog() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("GermanSheppard");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            Dog ex = new Dog();
+            ex.setVisible(true);
+        });
+    }
 }
+}
+
