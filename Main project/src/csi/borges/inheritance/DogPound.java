0bd4e63543ec3723cfda0c149a4d4530c021f9f2
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-17 08:51:38 -0400

    Update DogPound.java

--- Main project/src/csi/borges/inheritance/DogPound.java
+++ Main project/src/csi/borges/inheritance/DogPound.java
@@ -6,34 +6,71 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
-
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class DogPound extends JPanel {
 
-	 private void initDogPound() {
+	 private static final ActionEvent ActionEvent = null;
+	 private boolean inGame = true;
+	 
+	private void initDogPound() {
 
 	        addKeyListener(new TAdapter());
-	        setBackground(new Color(0, 131, 22));
+	        actionPerformed(ActionEvent);
 	        setFocusable(true); 
 
 	        setPreferredSize(new Dimension(WIDTH, HEIGHT));
 	        loadImages();
 	        initGame();
 	 }
-	        public void actionPerformed(ActionEvent e) {
+	        private void addKeyListener(TAdapter tAdapter) {
+		// TODO Auto-generated method stub
+		
+	}
+			private void loadImages() {
+		// TODO Auto-generated method stub
+		
+	}
+			private void initGame() {
+		// TODO Auto-generated method stub
+		
+	}
+			
+			public void actionPerformed(ActionEvent e) {
 
-	            if (inGame) {
+	            boolean inGame;
+				if (inGame) {
 	                checkCollision();
 	                // move();
 	            }
-	            
-	            @Override
+	        }
+	            private void checkCollision() {
+				// TODO Auto-generated method stub
+				
+			}
+				@Override
 	            public void paintComponent(Graphics g) {
-	                super.paintComponent(g);
-	                g.drawImage(background, 0, 0, null);
+	                super.paintComponent(g); 
 	                doDrawing(g);
-	            }       
+	            }  
+	            private void doDrawing(Graphics g) {
+					// TODO Auto-generated method stub
+					
+				}
+				private class TAdapter
+
+	            public void keyPressed(KeyEvent e) {
+
+	                int key = e.getKeyCode();
 	 }
 
 	private void checkCollision() {
@@ -46,7 +83,6 @@ public class DogPound extends JPanel {
 	}
 
 	private void loadImages() {
-		// TODO Auto-generated method stub
 		
 	}
 	    }
