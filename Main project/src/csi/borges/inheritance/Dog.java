ace4636bfb9c047a2246c2cf6b2baf4eb5f712b7
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-22 09:05:09 -0400

    Update Dog.java

--- Main project/src/csi/borges/inheritance/Dog.java
+++ Main project/src/csi/borges/inheritance/Dog.java
@@ -7,132 +7,108 @@ public class Dog {
 	int size;
 	boolean smelly;
 	boolean wild = false;
-	boolean gender= true; 
+	boolean gender = true;
 	private ImageIcon icon;
-	//int weight;
+	// int weight;
 
-	
-	
 //	constructors
 
-	
-	public Dog(String hair,int size,boolean smelly, boolean wild, boolean gender, String icon) {
+	public Dog(String hair, int size, boolean smelly, boolean wild, boolean gender, String icon) {
 		super();
 		this.hair = hair;
 		this.smelly = smelly;
-		//this.weight = weight;
+		// this.weight = weight;
 		this.size = size;
 		this.wild = wild;
 		this.gender = gender;
 		this.icon = new ImageIcon(getClass().getResource(icon));
 	}
-	
 
-	
 	public Dog() {
 
 	}
-	
-	
+
 //	functions
-	
-	public void piss() { 
-		if(gender==true) {
+
+	public void piss() {
+		if (gender == true) {
 			System.out.println("raise leg");
 		}
 		System.out.println("Tsssss");
-		
+
 	}
 
-
 	public Shit eat(Food f) {
-		
+
 		return new Shit();
 	}
 
 	public void die() {
 	}
+
 	public Noise bark(Noise n) {
-		
-		
+
 		return new Noise(n.decibels, n.recurring);
 	}
-	
+
 	void wagTail() {
 	}
 
 	public void mate(Dog D) {
 	}
-	
-	
-	
+	public void digest() {
+
+		return;
+	}
 //	classes
 
 	public class Shit {
 		boolean smelly;
-		//int weight; 
-		int size; 
+		// int weight;
+		int size;
 		String shape;
-		private Food f; 
-		
-		
-		
+		private Food f;
+
 		public Shit() {
 			return;
 		}
+
 		public Shit(boolean smelly, int size, String shape) {
 			super();
 			this.smelly = smelly;
-			//this.weight = weight; 
+			// this.weight = weight;
 			this.size = size;
 			this.shape = shape;
 		}
 	}
-	public void digest() {
-		
-		
-		
-		
-		return;
-	}
+
 	public class Food {
 		public Food(String flavor, String color, boolean dogFood) {
-			
+
 		}
+
 		String flavor;
 		String color;
 		boolean dogFood;
-		
 
 		public Food() {
-			
+
 		}
-		
+
 	}
 
 	public class Noise {
 		public Noise(int i, boolean b) {
-			
+
 		}
+
 		int decibels = 0;
 		boolean recurring = false;
 	}
-
-
-
-
-	public void setVisible(boolean b) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
 
 	public Noise bark() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
 
 }
