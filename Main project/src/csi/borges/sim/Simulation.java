86e47b2960b650b52ae8088a1f64a9f021b0369f
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-24 08:40:25 -0400

    Update Simulation.java

--- Main project/src/csi/borges/sim/Simulation.java
+++ Main project/src/csi/borges/sim/Simulation.java
@@ -1,12 +1,34 @@
-/**
- * 
- */
 package csi.borges.sim;
+	import java.awt.EventQueue;
+	import javax.swing.JFrame;
 
-/**
- * @author user
- *
- */
-public class Simulation {
+import csi.borges.inheritance.DogPound;
 
-}
+	public class Simulation extends JFrame {
+
+	    public Simulation() {
+	        
+	        initUI();
+	    }
+	    
+	    private void initUI() {
+	        
+	        add(new DogPound());
+	        
+	        setResizable(false);
+	        pack();
+	        
+	        setTitle("DogPound");
+	        setLocationRelativeTo(null);
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    }
+	    
+	    public static void main(String[] args) {
+	        
+	        EventQueue.invokeLater(() -> {
+	            JFrame ex = new Simulation();
+	            ex.setVisible(true);
+	        });
+	    }
+	}
+
