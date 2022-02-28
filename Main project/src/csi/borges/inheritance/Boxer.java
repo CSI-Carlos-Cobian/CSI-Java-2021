ffaa9143458d9ea7c563ba9ac52f5a177bb1db26
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-02-28 09:21:25 -0400

    Create Boxer.java

---
+++ Main project/src/csi/borges/inheritance/Boxer.java
@@ -0,0 +1,36 @@
+package csi.borges.inheritance;
+import javax.swing.ImageIcon;
+
+
+
+public class Boxer extends Dog {
+	
+	
+
+		public Boxer() {
+			super("White",69,true,false, true,"Boxer.png");
+//			this.size = 88;
+//			this.smelly = true;
+//			this.wild = false;
+//			this.hair = "golden brown";
+//			this.gender = true;
+//			this.icon = new ImageIcon(getClass().getResource("GermanSheppard.png"));
+		}
+	
+		@Override
+		public Noise bark() {
+
+			return new Noise(120, true);
+		}
+		public void wagTail() {
+			
+			System.out.println("Wiggle side to side");
+			
+		}
+	
+	public Shit eat() {
+
+		return new Shit(true, 3, "Chocolate");
+	}
+}
+
