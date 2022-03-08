0f05337e3b778cd10769eb86c844cca02eb25969
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-03-08 09:32:53 -0400

    Update Boxer.java

--- Main project/src/csi/borges/inheritance/Boxer.java
+++ Main project/src/csi/borges/inheritance/Boxer.java
@@ -27,10 +27,37 @@ public class Boxer extends Dog {
 			System.out.println("Wiggle side to side");
 			
 		}
-	
+		public class Food {
+			public Food(String flavor, String color, boolean humanFood) {
+				
+			}
+			public Food() {
+				// TODO Auto-generated constructor stub
+			}
+			String flavor;
+			String color;
+			boolean humanFood;
+			
+			public Shit digest() {
+				
+				return new Shit();
+			}
+		}
 	public Shit eat() {
 
 		return new Shit(true, 3, "Chocolate");
 	}
-}
 
+		public Noise bark() {
+			
+			return new Noise();
+		}
+		
+		public void mate(Dog d) {
+			
+		}
+		
+		public void grow() {
+			
+		}
+}
