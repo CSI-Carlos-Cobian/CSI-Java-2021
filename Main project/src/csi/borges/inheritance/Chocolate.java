e6e9ea6fda5d554f966433bb8edcae70015ac7f0
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-03-16 09:35:50 -0400

    Update Chocolate.java

--- Main project/src/csi/borges/inheritance/Chocolate.java
+++ Main project/src/csi/borges/inheritance/Chocolate.java
@@ -1,5 +1,18 @@
 package csi.borges.inheritance;
 
-public class Chocolate {
+public class Chocolate extends Dog.Food {
+
+    public int chocolate_x;
+    public int chocolate_y;
+    
+	public Chocolate(int chocolate_x, int chocolate_y) {
+		super();
+		this.chocolate_x = chocolate_x;
+		this.chocolate_y = chocolate_y;
+	}
+    
+
+	
+	
 
 }
