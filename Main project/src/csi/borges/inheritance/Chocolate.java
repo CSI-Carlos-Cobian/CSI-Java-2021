9e6cb4f44a7536d74a6a568016decbf3c215028b
CSI-Julio-Borges <89141121+CSI-Julio-Borges@users.noreply.github.com>
2022-03-25 09:19:09 -0400

    Update Chocolate.java

--- Main project/src/csi/borges/inheritance/Chocolate.java
+++ Main project/src/csi/borges/inheritance/Chocolate.java
@@ -1,22 +1,21 @@
 package csi.borges.inheritance;
 
+import java.awt.Point;
+
+import csi.borges.inheritance.Dog.Shit;
+
+
+
 public class Chocolate extends Dog.Food {
 
-//    public int chocolate_x;
-//    public int chocolate_y;
-//    
-	public Chocolate(int x, int y) {
-		new Dog().super();
-//		this.x = chocolate_x;
-//		this.chocolate_y = chocolate_y;
+	public Chocolate(Point p) {
+		new Dog().super(p);
+		this.chocolate=true;
 	}
-
-
-
-
-
-	public static void add(Chocolate chocolate) {
-		// TODO Auto-generated method stub
-		
+	
+	@Override
+	public Shit digest() {
+		new Dog().die();
+		return null;
 	}
 }
